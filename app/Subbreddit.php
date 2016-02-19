410a61200995f5d119055fba8c15ca9c730dfe4a
data-lee <ndoucet67@gmail.com>
2016-02-18 20:21:22 -0600

    Added Models

---
+++ app/Subbreddit.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subbreddit extends Model
+{
+    /**
+    * Get the user that owns the subbreddit.
+    */
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+
+    /**
+    * Get the posts of the subbreddit.
+    */
+    public function posts()
+    {
+    	return $this->hasMany('App\Post');
+    }
+
+    /**
+    * Get the subscribed users of the subreddit.
+    */
+    public function subscribedUsers()
+    {
+    	return $this->belongsToMany('App\User')->withTimestamps();
+    }
+
+}
